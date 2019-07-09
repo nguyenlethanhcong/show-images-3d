f46bcb61df356522604791b426c69a702b39072b
Nguyễn Lê Thành Công <33917105+nguyenlethanhcong@users.noreply.github.com>
2019-07-09 14:28:05 +0700

    a

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,43 @@
+function getRandomArbitrary(min, max){
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+$(document).ready(function(){
+	setInterval(function(){
+		var screenHeight = $(document).height();
+		var screenWidth = $(document).width();
+		var startLeft = getRandomArbitrary(0, screenWidth);
+		var timeRun = getRandomArbitrary(4000, 6000);
+		var opacityR = Math.random() * (1 - 0.2) + 0.2;
+		var sizeR = getRandomArbitrary(10, 40);
+		var endLeft = getRandomArbitrary(startLeft - 100, startLeft + 100);
+		var snow = document.createElement('span');
+
+		$(snow).addClass('snow-item fa fa-heart').css({
+			'position'  : 'absolute',
+			'z-index'   : 'auto',
+			'color'     : '#ff0000',
+			'display'   : 'block',
+			'top'       : 0,
+			'left'      : startLeft,
+			'opacity'   : opacityR,
+			'font-size' : sizeR+'px'
+		})
+		.appendTo('body')
+		.animate({
+			'top'       : screenHeight-sizeR,
+			'left'      : endLeft
+		},{
+			duration : timeRun,
+			easing : 'linear',
+			complete:function(){
+				$(this).fadeOut('fast',function(){
+					$(this).remove();
+				});
+			}
+		});
+	},500);
+});
 window.onload = function () {
 	var dds = document.getElementsByTagName('dd');
 	var dl = document.getElementsByTagName('dl')[0];
@@ -37,7 +77,7 @@ window.onload = function () {
 		}
 	}
 	function deal(dds, n) {
-		var speed = 100;
+		var speed = 500;
 		var translateZTerminus = 400;
 		var angle = 360 / dds.length * n;
 		var translateZ = 0;
